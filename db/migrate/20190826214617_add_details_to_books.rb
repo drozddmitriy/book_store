808e22cc9f1e1366e146799fed2387c8071f5639
drozd <drozd.dmitriy.etc@gmail.com>
2019-08-27 22:05:34 +0300

    Add migration

---
+++ db/migrate/20190826214617_add_details_to_books.rb
@@ -0,0 +1,10 @@
+class AddDetailsToBooks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :category_id, :integer
+    add_column :books, :year, :integer
+    add_column :books, :dimension_h, :decimal, precision: 5, scale: 2
+    add_column :books, :dimension_w, :decimal, precision: 5, scale: 2
+    add_column :books, :dimension_d, :decimal, precision: 5, scale: 2
+    add_column :books, :material, :string
+  end
+end
