e9cfc224e9d5ec2935958cab9159d577533a74ce
drozd <drozd.dmitriy.etc@gmail.com>
2019-08-28 15:58:15 +0300

    Refactoring views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @categories = Category.all
     
+    @books = Book.first(8)
   end
 
   def show
-
+    @book = Book.find_by(id: params[:id])
+    # binding.pry
   end
 end
