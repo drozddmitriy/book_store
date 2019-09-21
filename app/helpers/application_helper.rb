a9590d9e51b22fde41ca0a1a942c31ebc2c8170b
drozd <drozd.dmitriy.etc@gmail.com>
2019-09-21 16:54:20 +0300

    Fixed flash view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def alert_class(key)
+    alert_class = case key
+                   when 'notice' then "success"
+                   when 'alert' then "danger"
+                   else key
+                   end
+
+  end
 end
