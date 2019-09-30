7ec4b383373465774ddbb25c36e06620b6b63dc8
drozd <drozd.dmitriy.etc@gmail.com>
2019-09-30 16:59:47 +0300

    Add counter quantity

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.fa-minus').parent('#minus').click(function(event){
+    var input;
+    var quantity;
+    input = $(this).siblings('.quantity-input');
+    quantity = +input.val();
+    return input.val(quantity - 1);
+  });
+
+  $('.fa-plus').parent('#plus').click(function(event) {
+    var input;
+    var quantity;
+    input = $(this).siblings('.quantity-input');
+    quantity = +input.val();
+    return input.val(quantity + 1);
+  });
+});
