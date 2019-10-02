4433a5fca1cd7f960ab0ea9097b9984b19baaa0c
drozd <drozd.dmitriy.etc@gmail.com>
2019-10-02 22:10:41 +0300

    Add line_item_controller

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,44 @@
+class LineItemsController < ApplicationController
+  def index
+    @items = LineItem.all
+  end
+
+  def create
+    @line_item = LineItem.find_or_initialize_by(book_id: params[:line_item][:book_id])
+    @line_item.quantity = params[:line_item][:quantity]
+
+    if @line_item.save
+      flash[:success] = 'Item add to cart!'
+    else
+      flash[:danger] = 'Item not create!!!!!'
+     end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @line_item = LineItem.find_by(id: params[:id])
+
+    if @line_item.destroy
+      flash[:success] = 'Item delete'
+    else
+      flash[:danger] = 'Item not delete'
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def update
+    @line_item = LineItem.find_by(id: params[:id])
+    case params[:quantity]
+    when 'plus'
+      @line_item.quantity += 1
+    when 'minus'
+      @line_item.quantity -= 1 if @line_item.quantity > 1
+     end
+
+    @line_item.save
+
+    redirect_back(fallback_location: root_path)
+  end
+end
