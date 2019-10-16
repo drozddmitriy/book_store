d8191072df936c3fa0fe2209bea0508f5fe6b279
drozd <drozd.dmitriy.etc@gmail.com>
2019-10-16 23:08:29 +0300

    Add session_controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Users::SessionsController < Devise::SessionsController
+  def new
+     super
+   end
+
+   def create
+     super
+   end
+
+   def after_sign_in_path_for(resource)
+     if current_order.line_items.any?
+       checkout_path(:addresses)
+     else
+       super
+     end
+   end
+
+   def after_sign_out_path_for(resource)
+     current_order.destroy
+     super
+   end
+end
