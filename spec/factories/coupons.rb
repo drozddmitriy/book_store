d3d02aa307a439c9d59640b4ce4e346ca8bd5bec
drozd <drozd.dmitriy.etc@gmail.com>
2019-11-11 01:34:01 +0200

    Add circleci

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :coupon do
-    # coupon { Array.new(6){ Array('A'..'Z').sample }.join }
     coupon { 'COUPON' }
     sale { rand(1..99) }
     active { true }
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :order do
     number { Array.new(6){ Array('A'..'Z').sample }.join }
     total_price { rand(1..1000) }
+    status { 'in_queue' }
+    use_billing { true }
     association :user
   end
 end
