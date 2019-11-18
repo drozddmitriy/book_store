b0b7507b4e7e9c0a4fec466dd50dd17eff913251
drozd <drozd.dmitriy.etc@gmail.com>
2019-11-18 20:48:46 +0200

    Fixed checkout login views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :current_password
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:facebook]
@@ -9,7 +8,7 @@ class User < ApplicationRecord
   has_many :addresses, as: :addressable, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :orders, dependent: :destroy
-  has_many :line_items, dependent: :destroy
+  has_many :line_items, through: :orders, dependent: :destroy
   has_one :credit_card, dependent: :destroy
 
   def self.new_with_session(params, session)
