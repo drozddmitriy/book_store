279062374b13d6a0a3b5dfc39f4f4d7935853ee0
drozd <drozd.dmitriy.etc@gmail.com>
2020-01-16 11:04:41 +0200

    Fixed specs

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :address do |address|
-    first_name { 'FirstName' }
-    last_name { 'LastName' }
-    address { 'Suchkova' }
-    city { 'Novomoskovsk' }
+    first_name { FFaker::Lorem.word }
+    last_name { FFaker::Lorem.word }
+    address { FFaker::Lorem.word }
+    city { FFaker::Lorem.word }
     zip { FFaker::AddressDE.zip_code }
     country { FFaker::Address.country_code }
     phone { '+380976245499' }
--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe OrderMailer, type: :mailer do
     let(:user) { create(:user) }
     let(:mail) { described_class.confirm_order(user) }
 
-    it { expect(mail.subject).to eq('Your Order with bookstore.com') }
+    it { expect(mail.subject).to eq(I18n.t('order_mailer.message')) }
     it { expect(mail.to).to eq([user.email]) }
     it { expect(mail.from).to eq(['from@example.com']) }
-    it { expect(mail.body.encoded).to match("Hi #{user.email}!") }
+    it { expect(mail.body.encoded).to match(I18n.t('order_mailer.hi', email: user.email)) }
   end
 end
